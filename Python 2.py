8873cdf8c75a2b22737fbf488adda710c0949ee6
ALBENAVIDES22 <91989070+ALBENAVIDES22@users.noreply.github.com>
2021-10-22 15:30:42 -0500

    Add files via upload

---
+++ Python 2.py
@@ -0,0 +1,17 @@
+##
+# Sample Program that demonstrates the print function
+#
+# Prints 7
+
+
+print(3 + 4)
+
+# Print Hello World! on two lines
+print("Hello")
+print("World!")
+
+# print multiple values with a single print function call
+print("My favortie number are", 3 + 4, "and", 3 + 10)
+
+# Print Hello World! on two lines
+print("Goodbye")
